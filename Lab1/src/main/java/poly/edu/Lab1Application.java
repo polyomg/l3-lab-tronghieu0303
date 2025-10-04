87bda3d08eec705e371f3fa83dc5288089bc1041
tronghieu0303 <hieuptps39685@gmail.com>
2025-10-04 13:11:25 +0700

    PS39685-PhanTrongHieu-lab1

--- Lab1/src/main/java/poly/edu/Lab1Application.java
+++ Lab1/src/main/java/poly/edu/Lab1Application.java
@@ -1,13 +1,11 @@
-package com.poly.lab1;
+package poly.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Lab1Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Lab1Application.class, args);
     }
-
 }
